e2909fe9e67b9ef199399d998784e77903e5eac2
santiagocolombi <148695829+santiagocolombi@users.noreply.github.com>
2024-10-02 19:07:43 -0300

    feat(): changing names of pages routes

--- src/assets/Login.jsx
+++ src/assets/Login.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 function Login(){
     return <div>
-        <h1>Login</h1>
-        <Link to ={'/Produto'}>Produto</Link>
+        <h1>Acervo de minerais</h1>
+        <Link to ={'/Produto'}>Lista de minerais</Link>
         
     </div>;
 }
--- src/assets/Produto.jsx
+++ src/assets/Produto.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 function Produto() {
     return <div>
-        <h1>Produto</h1>
-        <Link to = {'/Produto'}>oi</Link>
+        
+        <Link to = {'/'}>Voltar</Link>
         
     </div>;
 }
