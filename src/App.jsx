422b259913f4fb6bfc9564c2063693391920a3d6
santiagocolombi <148695829+santiagocolombi@users.noreply.github.com>
2024-10-02 17:36:46 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Login from './assets/Login'
+import Produto from './assets/Produto'
+
+function App() {
+
+  return ( 
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/Produto' element={<Produto/>}/>
+      <Route path='*' element={<h1>Não encontrada</h1>}/> 
+    </Routes>
+    </BrowserRouter>
+
+  )
+}
+
+export default App
---
+++ src/assets/Produto.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+function Produto() {
+    return <div>
+        <h1>Produto</h1>
+        <Link to = {'/Produto'}>oi</Link>
+        
+    </div>;
+}
+
+export default Produto;
